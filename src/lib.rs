f0137ebb0fab0ceb7d370e50f02ed6f35db3a50f
AlexMa178 <alexcma27@gmail.com>
2026-08-16 13:27:08 -0700

    changed Rot to Angle

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,16 @@ pub use ray::*;
 use std::ops::{ Add, AddAssign, Mul, MulAssign, Neg, Sub, SubAssign };
 use std::f32::consts::{ TAU };
 
-/// A Rot represents an angle.
+/// Angle<N> represents an angle where the circle is subdivided in N parts.
 /// `N` may not be zero.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-pub struct Rot<const N: u16> {
+pub struct Angle<const N: u16> {
     n: u16,
 }
-impl<const N: u16> Rot<N> {
+impl<const N: u16> Angle<N> {
 
-    pub fn all() -> Vec<Rot<N>> {
-        (0..N).map(|i| Rot { n: i }).collect()
+    pub fn all() -> Vec<Angle<N>> {
+        (0..N).map(|i| Angle { n: i }).collect()
     }
     
     /// Gives None if `angle`, once normalized to `0..=359`, is not a multiple of `N`.
@@ -22,14 +22,14 @@ impl<const N: u16> Rot<N> {
         if norm_angle.is_multiple_of(N) { Some(Self { n: norm_angle / N }) } else { None }
     }
 
-    /// Snaps to the closest valid `Rot<N>`.
+    /// Snaps to the closest valid `Angle<N>`.
     pub const fn from_deg(angle: f32) -> Self {
         let quantize = (angle / (360. / N as f32)).round() as i32;
         let normalize = quantize.rem_euclid(N as i32) as u16;
         Self { n: normalize }
     }
 
-    /// Snaps to the closest valid `Rot<N>`.
+    /// Snaps to the closest valid `Angle<N>`.
     pub const fn from_rad(angle: f32) -> Self {
         let quantize = (angle / (TAU / N as f32)).round() as i32;
         let normalize = quantize.rem_euclid(N as i32) as u16;
@@ -52,63 +52,63 @@ impl<const N: u16> Rot<N> {
     }
 
     /// Gives `None` if `self` cannot be divided.
-    /// For example: `Rot::R8_90.split_as::<4>()` gives `Some(Rot::R4_90)`, but `Rot::R8_45.split_as::<4>()` gives `None`.
+    /// For example: `Angle::A8_90.split_as::<4>()` gives `Some(Angle::RA_90)`, but `Angle::A8_45.split_as::<4>()` gives `None`.
     /// # Panics
     /// Panics if `N` is not a multiple of `M`.
-    pub const fn split_as<const M: u16>(self) -> Option<Rot<M>> {
+    pub const fn split_as<const M: u16>(self) -> Option<Angle<M>> {
         if !N.is_multiple_of(M) { panic!("failed split_as"); }
-        if self.n.is_multiple_of(N / M) { Some(Rot { n: self.n / (N / M) }) } else { None }
+        if self.n.is_multiple_of(N / M) { Some(Angle { n: self.n / (N / M) }) } else { None }
     }
 
     /// # Panics
     /// Panics if `M` is not a multiple of `N`.
-    pub const fn embed_as<const M: u16>(self) -> Rot<M> {
+    pub const fn embed_as<const M: u16>(self) -> Angle<M> {
         if !M.is_multiple_of(N) { panic!("failed embed_as"); }
-        Rot { n: self.n * (M / N) }
+        Angle { n: self.n * (M / N) }
     }
 
 }
-impl Rot<4> {
+impl Angle<4> {
 
-    pub const R4_0  : Self = Self { n: 0 };
-    pub const R4_90 : Self = Self { n: 1 };
-    pub const R4_180: Self = Self { n: 2 };
-    pub const R4_270: Self = Self { n: 3 };
+    pub const A4_0  : Self = Self { n: 0 };
+    pub const A4_90 : Self = Self { n: 1 };
+    pub const A4_180: Self = Self { n: 2 };
+    pub const A4_270: Self = Self { n: 3 };
 
-    /// Useful for doing an exhaustive match on a `Rot<4>`.
-    pub const fn matchable_4(self) -> R4 {
+    /// Useful for doing an exhaustive match on a `Angle<4>`.
+    pub const fn matchable_4(self) -> A4 {
         match self.n {
-            0 => R4::D0,
-            1 => R4::D90,
-            2 => R4::D180,
-            3 => R4::D270,
+            0 => A4::D0,
+            1 => A4::D90,
+            2 => A4::D180,
+            3 => A4::D270,
             _ => unreachable!(),
         }
     }
 
 }
-impl Rot<8> {
+impl Angle<8> {
 
-    pub const R8_0  : Self = Self { n: 0 };
-    pub const R8_45 : Self = Self { n: 1 };
-    pub const R8_90 : Self = Self { n: 2 };
-    pub const R8_135: Self = Self { n: 3 };
-    pub const R8_180: Self = Self { n: 4 };
-    pub const R8_225: Self = Self { n: 5 };
-    pub const R8_270: Self = Self { n: 6 };
-    pub const R8_315: Self = Self { n: 7 };
+    pub const A8_0  : Self = Self { n: 0 };
+    pub const A8_45 : Self = Self { n: 1 };
+    pub const A8_90 : Self = Self { n: 2 };
+    pub const A8_135: Self = Self { n: 3 };
+    pub const A8_180: Self = Self { n: 4 };
+    pub const A8_225: Self = Self { n: 5 };
+    pub const A8_270: Self = Self { n: 6 };
+    pub const A8_315: Self = Self { n: 7 };
 
-    /// Useful for doing an exhaustive match on a `Rot<8>`.
-    pub const fn matchable_8(self) -> R8 {
+    /// Useful for doing an exhaustive match on a `Angle<8>`.
+    pub const fn matchable_8(self) -> A8 {
         match self.n {
-            0 => R8::D0,
-            1 => R8::D45,
-            2 => R8::D90,
-            3 => R8::D135,
-            4 => R8::D180,
-            5 => R8::D225,
-            6 => R8::D270,
-            7 => R8::D315,
+            0 => A8::D0,
+            1 => A8::D45,
+            2 => A8::D90,
+            3 => A8::D135,
+            4 => A8::D180,
+            5 => A8::D225,
+            6 => A8::D270,
+            7 => A8::D315,
             _ => unreachable!(),
         }
     }
@@ -116,69 +116,69 @@ impl Rot<8> {
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-pub enum R4 {
+pub enum A4 {
     D0, D90, D180, D270
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-pub enum R8 {
+pub enum A8 {
     D0, D45, D90, D135, D180, D225, D270, D315
 }
 
-const fn neg_const<const N: u16>(r: Rot<N>) -> Rot<N> {
-    Rot { n: (N - r.n) % N }
+const fn neg_const<const N: u16>(r: Angle<N>) -> Angle<N> {
+    Angle { n: (N - r.n) % N }
 }
-const fn add_const<const N: u16>(a: Rot<N>, b: Rot<N>) -> Rot<N> {
-    Rot { n: (a.n + b.n) % N }
+const fn add_const<const N: u16>(a: Angle<N>, b: Angle<N>) -> Angle<N> {
+    Angle { n: (a.n + b.n) % N }
 }
-const fn sub_const<const N: u16>(a: Rot<N>, b: Rot<N>) -> Rot<N> {
-    Rot { n: (a.n + N - b.n) % N }
+const fn sub_const<const N: u16>(a: Angle<N>, b: Angle<N>) -> Angle<N> {
+    Angle { n: (a.n + N - b.n) % N }
 }
-const fn mul_const<const N: u16>(r: Rot<N>, factor: u16) -> Rot<N> {
-    Rot { n: (r.n * factor) % N }
+const fn mul_const<const N: u16>(r: Angle<N>, factor: u16) -> Angle<N> {
+    Angle { n: (r.n * factor) % N }
 }
 
-impl<const N: u16> Neg for Rot<N> {
+impl<const N: u16> Neg for Angle<N> {
     type Output = Self;
     fn neg(self) -> Self::Output {
         neg_const::<N>(self)
     }
 }
-impl<const N: u16> Add for Rot<N> {
+impl<const N: u16> Add for Angle<N> {
     type Output = Self;
     fn add(self, rhs: Self) -> Self::Output {
         add_const::<N>(self, rhs)
     }
 }
-impl<const N: u16> AddAssign for Rot<N> {
+impl<const N: u16> AddAssign for Angle<N> {
     fn add_assign(&mut self, rhs: Self) {
         *self = add_const::<N>(*self, rhs);
     }
 }
-impl<const N: u16> Sub for Rot<N> {
+impl<const N: u16> Sub for Angle<N> {
     type Output = Self;
     fn sub(self, rhs: Self) -> Self::Output {
         sub_const::<N>(self, rhs)
     }
 }
-impl<const N: u16> SubAssign for Rot<N> {
+impl<const N: u16> SubAssign for Angle<N> {
     fn sub_assign(&mut self, rhs: Self) {
         *self = sub_const::<N>(*self, rhs);
     }
 }
-impl<const N: u16> Mul<u16> for Rot<N> {
-    type Output = Rot<N>;
+impl<const N: u16> Mul<u16> for Angle<N> {
+    type Output = Angle<N>;
     fn mul(self, rhs: u16) -> Self::Output {
         mul_const::<N>(self, rhs)
     }
 }
-impl<const N: u16> Mul<Rot<N>> for u16 {
-    type Output = Rot<N>;
-    fn mul(self, rhs: Rot<N>) -> Self::Output {
+impl<const N: u16> Mul<Angle<N>> for u16 {
+    type Output = Angle<N>;
+    fn mul(self, rhs: Angle<N>) -> Self::Output {
         mul_const::<N>(rhs, self)
     }
 }
-impl<const N: u16> MulAssign<u16> for Rot<N> {
+impl<const N: u16> MulAssign<u16> for Angle<N> {
     fn mul_assign(&mut self, rhs: u16) {
         *self = mul_const::<N>(*self, rhs);
     }
--- src/ray.rs
+++ src/ray.rs
@@ -1,7 +1,7 @@
 use std::cmp::Ordering;
 use std::ops::{ Add, AddAssign, Neg, Sub, SubAssign };
 
-use crate::{ Rot, add_const, neg_const, sub_const };
+use crate::{ Angle, add_const, neg_const, sub_const };
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum RotFrom {
@@ -10,13 +10,13 @@ pub enum RotFrom {
 
 impl RotFrom {
 
-    pub const fn angle_to(self, other: RotFrom, dir: RotDir) -> Rot<4> {
-        const fn from_pos_x_ccw(f: RotFrom) -> Rot<4> {
+    pub const fn angle_to(self, other: RotFrom, dir: RotDir) -> Angle<4> {
+        const fn from_pos_x_ccw(f: RotFrom) -> Angle<4> {
             match f {
-                RotFrom::PosX => Rot::R4_0,
-                RotFrom::PosY => Rot::R4_90,
-                RotFrom::NegX => Rot::R4_180,
-                RotFrom::NegY => Rot::R4_270,
+                RotFrom::PosX => Angle::A4_0,
+                RotFrom::PosY => Angle::A4_90,
+                RotFrom::NegX => Angle::A4_180,
+                RotFrom::NegY => Angle::A4_270,
             }
         }
         let diff_ccw = sub_const(from_pos_x_ccw(self), from_pos_x_ccw(other));
@@ -46,13 +46,13 @@ impl RotDir {
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 /// A Ray represents a direction in 2d cartesian space.
 pub struct Ray<const N: u16> {
-    pos_x_ccw: Rot<N>
+    pos_x_ccw: Angle<N>
 }
 impl<const N: u16> Ray<N> {
 
     /// # Panics
     /// Panics if `N` is not a multiple of `4`.
-    pub const fn new(rot: Rot<N>, from: RotFrom, dir: RotDir) -> Self {
+    pub const fn new(rot: Angle<N>, from: RotFrom, dir: RotDir) -> Self {
         let d = from.angle_to(RotFrom::PosX, dir).embed_as::<N>();
         Self { pos_x_ccw: match dir {
             RotDir::Clockwise => sub_const(d, rot),
@@ -62,7 +62,7 @@ impl<const N: u16> Ray<N> {
 
     /// # Panics
     /// Panics if `N` is not a multiple of `4`.
-    pub const fn rot(self, from: RotFrom, dir: RotDir) -> Rot<N> {
+    pub const fn rot(self, from: RotFrom, dir: RotDir) -> Angle<N> {
         let d = RotFrom::PosX.angle_to(from, RotDir::CounterClockwise).embed_as::<N>();
         match dir {
             RotDir::Clockwise => sub_const(d, self.pos_x_ccw),
@@ -77,14 +77,14 @@ impl<const N: u16> Ray<N> {
     pub const fn from_signs(dx: Ordering, dy: Ordering) -> Option<Self> {
         use { Ordering::Less as Neg, Ordering::Equal as Zer, Ordering::Greater as Pos };
         let angle_8 = match (dx, dy) {
-            (Pos, Zer) => Rot::R8_0,
-            (Pos, Pos) => Rot::R8_45, 
-            (Zer, Pos) => Rot::R8_90,
-            (Neg, Pos) => Rot::R8_135,
-            (Neg, Zer) => Rot::R8_180,
-            (Neg, Neg) => Rot::R8_225,
-            (Zer, Neg) => Rot::R8_270,
-            (Pos, Neg) => Rot::R8_315,
+            (Pos, Zer) => Angle::A8_0,
+            (Pos, Pos) => Angle::A8_45, 
+            (Zer, Pos) => Angle::A8_90,
+            (Neg, Pos) => Angle::A8_135,
+            (Neg, Zer) => Angle::A8_180,
+            (Neg, Neg) => Angle::A8_225,
+            (Zer, Neg) => Angle::A8_270,
+            (Pos, Neg) => Angle::A8_315,
             (Zer, Zer) => return None,
         };
         match angle_8.split_as::<N>() {
@@ -94,13 +94,13 @@ impl<const N: u16> Ray<N> {
     }
 
     /// Gives `None` is `dx` and `dy` are `0`.
-    /// Snaps to the closest valid `Rot<N>`.
+    /// Snaps to the closest valid `Angle<N>`.
     /// # Panics
     /// Panics if `N` is not a multiple of `4`.
     pub fn from_vector(dx: f32, dy: f32) -> Option<Self> {
         assert!(N.is_multiple_of(4));
         if f32::hypot(dx, dy) <= f32::EPSILON { return None; }
-        Some(Self { pos_x_ccw: Rot::<N>::from_rad(f32::atan2(dy, dx)) })
+        Some(Self { pos_x_ccw: Angle::<N>::from_rad(f32::atan2(dy, dx)) })
     }
 
     pub fn unit_vector(self) -> [ f32; 2 ] {
@@ -116,31 +116,31 @@ impl<const N: u16> Neg for Ray<N> {
         Ray { pos_x_ccw: neg_const(self.pos_x_ccw) }
     }
 }
-impl<const N: u16> Add<Rot<N>> for Ray<N> {
+impl<const N: u16> Add<Angle<N>> for Ray<N> {
     type Output = Ray<N>;
-    fn add(self, rhs: Rot<N>) -> Self::Output {
+    fn add(self, rhs: Angle<N>) -> Self::Output {
         Ray { pos_x_ccw: add_const(self.pos_x_ccw, rhs) }
     }
 }
-impl<const N: u16> Add<Ray<N>> for Rot<N> {
+impl<const N: u16> Add<Ray<N>> for Angle<N> {
     type Output = Ray<N>;
     fn add(self, rhs: Ray<N>) -> Self::Output {
         Ray { pos_x_ccw: add_const(self, rhs.pos_x_ccw) }
     }
 }
-impl<const N: u16> AddAssign<Rot<N>> for Ray<N> {
-    fn add_assign(&mut self, rhs: Rot<N>) {
+impl<const N: u16> AddAssign<Angle<N>> for Ray<N> {
+    fn add_assign(&mut self, rhs: Angle<N>) {
         self.pos_x_ccw = add_const(self.pos_x_ccw, rhs);
     }
 }
-impl<const N: u16> Sub<Rot<N>> for Ray<N> {
+impl<const N: u16> Sub<Angle<N>> for Ray<N> {
     type Output = Ray<N>;
-    fn sub(self, rhs: Rot<N>) -> Self::Output {
+    fn sub(self, rhs: Angle<N>) -> Self::Output {
         Ray { pos_x_ccw: sub_const(self.pos_x_ccw, rhs) }
     }
 }
-impl<const N: u16> SubAssign<Rot<N>> for Ray<N> {
-    fn sub_assign(&mut self, rhs: Rot<N>) {
+impl<const N: u16> SubAssign<Angle<N>> for Ray<N> {
+    fn sub_assign(&mut self, rhs: Angle<N>) {
         self.pos_x_ccw = sub_const(self.pos_x_ccw, rhs);
     }
 }
